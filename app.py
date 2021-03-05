f10f4dabb3015cc6a3835d8a3c93f4e086e53ee2
Oisin Tohak <oisintohak@gmail.com>
2021-03-05 12:09:45 +0530

    add edit category functionality

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ def add_task():
         mongo.db.tasks.insert_one(task)
         flash("Task Successfully Added")
         return redirect(url_for("get_tasks"))
-        
+
     categories = mongo.db.categories.find().sort("category_name", 1)
     return render_template("add_task.html", categories=categories)
 
@@ -124,12 +124,12 @@ def edit_task(task_id):
             "due_date": request.form.get("due_date"),
             "created_by": session["user"]
         }
-        mongo.db.tasks.update({"_.id": ObjectId(task_id)}, submit)
+        mongo.db.tasks.update({"_id": ObjectId(task_id)}, submit)
         flash("Task Successfully Updated")
 
     task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
     categories = mongo.db.categories.find().sort("category_name", 1)
-    return render_template("edit_task.html",task=task, categories=categories)
+    return render_template("edit_task.html", task=task, categories=categories)
 
 
 @app.route("/delete_task/<task_id>")
@@ -152,8 +152,22 @@ def add_category():
         mongo.db.categories.insert_one({"category_name": new_category})
         flash("Category Successfully Added")
         return redirect(url_for("get_categories"))
-    
+
     return render_template("add_category.html")
+
+
+@app.route("/edit_category/<category_id>", methods=["GET", "POST"])
+def edit_category(category_id):
+    if request.method == "POST":
+        submit = {
+            "category_name": request.form.get("category_name")
+        }
+        mongo.db.categories.update({"_id": ObjectId(category_id)}, submit)
+        flash("Category Updated Successfully")
+        return redirect(url_for("get_categories"))
+    category = mongo.db.categories.find_one({"_id": ObjectId(category_id)})
+    return render_template("edit_category.html", category=category)
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
